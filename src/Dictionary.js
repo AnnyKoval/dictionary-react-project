07bc0c7583a42abaec5ee38c27c47cc6476dab87
Anna Koval <106483373+AnnyKoval@users.noreply.github.com>
2022-08-20 15:39:54 +0300

    added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  function search(event) {
-    event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
 
+  function search(event) {
+    event.preventDefault();
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiURL).then(handleResponse);
+  }
   function handleKeywordChange(event) {
     console.log(event.target.value);
     setKeyword(event.target.value);
