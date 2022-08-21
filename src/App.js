3502cf20f00aae61dcaf85fd380c7377c2144408
Anna Koval <106483373+AnnyKoval@users.noreply.github.com>
2022-08-21 15:32:42 +0300

    updates CSS

--- src/App.js
+++ src/App.js
@@ -14,15 +14,27 @@ export default function App() {
         <header className="App-header">
           <div className="row">
             <div className="col">
-              <img src={logo} className="App-logo img-fluid" alt="logo" />
-              <img src={logo2} className="App-logo img-fluid" alt="logo2" />
-              <img src={logo3} className="App-logo img-fluid" alt="logo3" />
+              <img src={logo} className="App-logo img-fluid logo1" alt="logo" />
+              <img
+                src={logo2}
+                className="App-logo img-fluid logo2"
+                alt="logo2"
+              />
+              <img
+                src={logo3}
+                className="App-logo img-fluid logo3"
+                alt="logo3"
+              />
               <img
                 src={logo4}
-                className="App-logo img-fluid"
+                className="App-logo img-fluid logo4"
                 alt="logo4"
               />{" "}
-              <img src={logo5} className="App-logo img-fluid" alt="logo5" />
+              <img
+                src={logo5}
+                className="App-logo img-fluid logo5"
+                alt="logo5"
+              />
             </div>
           </div>
         </header>
